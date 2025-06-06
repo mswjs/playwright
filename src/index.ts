d8ad50fcc8f8941dc2937119710622db3e641bdf
Artem Zakharchenko <kettanaito@gmail.com>
2025-06-06 19:12:22 +0200

    feat: support WebSockets

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
-import type { Page, TestFixture } from '@playwright/test'
+import type { Page, TestFixture, WebSocketRoute } from '@playwright/test'
 import {
   type LifeCycleEventsMap,
   SetupApi,
   RequestHandler,
   getResponse,
+  WebSocketHandler,
 } from 'msw'
+import {
+  type WebSocketClientEventMap,
+  type WebSocketData,
+  type WebSocketServerEventMap,
+  WebSocketClientConnectionProtocol,
+  WebSocketServerConnectionProtocol,
+} from '@mswjs/interceptors/WebSocket'
 
 export interface CreateWorkerFixtureArgs {
   initialHandlers: Array<RequestHandler>
@@ -35,6 +43,7 @@ export class WorkerFixture extends SetupApi<LifeCycleEventsMap> {
   }
 
   public async start() {
+    // Handle HTTP requests.
     await this.#page.route(/.+/, async (route, request) => {
       const fetchRequest = new Request(request.url(), {
         method: request.method(),
@@ -62,10 +71,199 @@ export class WorkerFixture extends SetupApi<LifeCycleEventsMap> {
 
       route.continue()
     })
+
+    // Handle WebSocket connections.
+    this.#page.routeWebSocket(/.+/, async (ws) => {
+      const allWebSocketHandlers = this.handlersController
+        .currentHandlers()
+        .filter((handler) => {
+          return handler instanceof WebSocketHandler
+        })
+
+      if (allWebSocketHandlers.length === 0) {
+        ws.connectToServer()
+        return
+      }
+
+      const client = new PlaywrightWebSocketClientConnection(ws)
+      const server = new PlaywrightWebSocketServerConnection(ws)
+
+      for (const handler of allWebSocketHandlers) {
+        handler.run({
+          client,
+          server,
+          info: { protocols: [] },
+        })
+      }
+    })
   }
 
   public async stop() {
     super.dispose()
     await this.#page.unroute(/.+/)
+  }
+}
+
+class PlaywrightWebSocketClientConnection
+  implements WebSocketClientConnectionProtocol
+{
+  public id: string
+  public url: URL
+
+  constructor(protected readonly ws: WebSocketRoute) {
+    this.id = crypto.randomUUID()
+    this.url = new URL(ws.url())
+  }
+
+  public send(data: WebSocketData): void {
+    /**
+     * @note Playwright type definitions are tailored to Node.js
+     * while MSW describes all data types that can be sent over
+     * the WebSocket protocol.
+     */
+    this.ws.send(data as any)
+  }
+
+  public close(code?: number, reason?: string): void {
+    this.ws.close({ code, reason })
+  }
+
+  public addEventListener<EventType extends keyof WebSocketClientEventMap>(
+    type: EventType,
+    listener: (
+      this: WebSocket,
+      event: WebSocketClientEventMap[EventType],
+    ) => void,
+    options?: AddEventListenerOptions | boolean,
+  ): void {
+    /**
+     * @note Playwright does not expose the actual WebSocket reference.
+     */
+    const target = {} as WebSocket
+
+    if (typeof options === 'object' && options?.once) {
+      console.warn(
+        '@msw/playwright: WebSocketRoute from Playwright does not support "once" option',
+      )
+    }
+
+    switch (type) {
+      case 'message': {
+        this.ws.onMessage((data) => {
+          listener.call(
+            target,
+            new MessageEvent('message', {
+              data,
+            }) as any,
+          )
+        })
+        break
+      }
+
+      case 'close': {
+        this.ws.onClose((code, reason) => {
+          listener.call(
+            target,
+            new CloseEvent('close', { code, reason }) as any,
+          )
+        })
+        break
+      }
+    }
+  }
+
+  public removeEventListener<EventType extends keyof WebSocketClientEventMap>(
+    event: EventType,
+    listener: (
+      this: WebSocket,
+      event: WebSocketClientEventMap[EventType],
+    ) => void,
+    options?: EventListenerOptions | boolean,
+  ): void {
+    console.warn(
+      '@msw/playwright: WebSocketRoute does not support removing event listeners',
+    )
+  }
+}
+
+class PlaywrightWebSocketServerConnection
+  implements WebSocketServerConnectionProtocol
+{
+  #server?: WebSocketRoute
+
+  constructor(protected readonly ws: WebSocketRoute) {}
+
+  public connect(): void {
+    this.#server = this.ws.connectToServer()
+  }
+
+  public send(data: WebSocketData): void {
+    if (!this.#server) {
+      //
+      return
+    }
+
+    this.#server.send(data as any)
+  }
+
+  public close(code?: number, reason?: string): void {
+    if (!this.#server) {
+      //
+      return
+    }
+
+    this.#server.close({ code, reason })
+  }
+
+  public addEventListener<EventType extends keyof WebSocketServerEventMap>(
+    event: EventType,
+    listener: (
+      this: WebSocket,
+      event: WebSocketServerEventMap[EventType],
+    ) => void,
+    options?: AddEventListenerOptions | boolean,
+  ): void {
+    if (!this.#server) {
+      return
+    }
+
+    if (typeof options === 'object' && options?.once) {
+      console.warn(
+        '@msw/playwright: WebSocketRoute from Playwright does not support "once" option',
+      )
+    }
+
+    const target = {} as WebSocket
+    switch (event) {
+      case 'message': {
+        this.#server.onMessage((data) => {
+          listener.call(target, new MessageEvent('message', { data }) as any)
+        })
+        break
+      }
+
+      case 'close': {
+        this.#server.onClose((code, reason) => {
+          listener.call(
+            target,
+            new CloseEvent('close', { code, reason }) as any,
+          )
+        })
+        break
+      }
+    }
+  }
+
+  public removeEventListener<EventType extends keyof WebSocketServerEventMap>(
+    event: EventType,
+    listener: (
+      this: WebSocket,
+      event: WebSocketServerEventMap[EventType],
+    ) => void,
+    options?: EventListenerOptions | boolean,
+  ): void {
+    console.warn(
+      '@msw/playwright: WebSocketRoute does not support removing event listeners',
+    )
   }
 }
---
+++ tests/websockets.test.ts
@@ -0,0 +1,36 @@
+import { test as testBase, expect } from '@playwright/test'
+import { ws } from 'msw'
+import { createWorkerFixture, type WorkerFixture } from '../src/index.js'
+
+interface Fixtures {
+  worker: WorkerFixture
+}
+
+const test = testBase.extend<Fixtures>({
+  worker: createWorkerFixture(),
+})
+
+const api = ws.link('ws://localhost/api')
+
+test('sends a mocked event to the client', async ({ worker, page }) => {
+  worker.use(
+    api.addEventListener('connection', ({ client }) => {
+      console.log('WS CONNECTION!')
+
+      client.send('hello world')
+    }),
+  )
+
+  const message = await page.evaluate(() => {
+    const ws = new WebSocket('ws://localhost/api')
+
+    return new Promise<string>((resolve, reject) => {
+      ws.onerror = () => reject(new Error('WebSocket connection failed'))
+      ws.onmessage = (event) => {
+        resolve(event.data)
+      }
+    })
+  })
+
+  expect(message).toBe('hello world')
+})
