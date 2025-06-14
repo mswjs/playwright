c06c89a9e5566653a3facadc27faa3cbc632aee9
Artem Zakharchenko <kettanaito@gmail.com>
2025-06-14 13:41:35 +0200

    test: add overrides test case

---
+++ tests/overrides.test.ts
@@ -0,0 +1,47 @@
+import { test as testBase, expect } from '@playwright/test'
+import { http } from 'msw'
+import { createWorkerFixture, type WorkerFixture } from '../src/index.js'
+
+interface Fixtures {
+  worker: WorkerFixture
+}
+
+const test = testBase.extend<Fixtures>({
+  worker: createWorkerFixture(),
+})
+
+test.beforeEach(({ worker }) => {
+  worker.use(
+    http.get('*', () => {
+      return new Response('fallback')
+    }),
+  )
+})
+
+test('responds with the override mocked response', async ({ worker, page }) => {
+  worker.use(
+    http.get('*/resource', () => {
+      return new Response('hello world')
+    }),
+  )
+
+  await page.goto('')
+  const data = await page.evaluate(() => {
+    return fetch('http://localhost/resource').then((response) => {
+      return response.text()
+    })
+  })
+
+  expect(data).toBe('hello world')
+})
+
+test('responds with a fallback response', async ({ page }) => {
+  await page.goto('')
+  const data = await page.evaluate(() => {
+    return fetch('http://localhost/intentionally-unknown').then((response) => {
+      return response.text()
+    })
+  })
+
+  expect(data).toBe('fallback')
+})
